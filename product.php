4a6f1548abd2106200a8f30c103f05bb0b688322
Andy-of-Indiana <99208998+AndrewOfIndana@users.noreply.github.com>
2022-03-31 16:30:49 -0400

    needed php files

---
+++ product.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- 
+            Authors: Andrew Choi, Abraham Johnson
+            Date: INSERT NEW DATE
+        -->
+        <meta charset="utf-8">
+        <title>Electronic's Store</title>
+        <link type="text/css" rel="stylesheet" href="www/css/styles.css" />
+    </head>
+
+    <body>
+        <div id="navbar">
+            <a href="index.html">Home</a>  || <a href="books.html">List Books</a> ||
+        </div>
+
+         <!-- Insert body !-->
+
+        <div id="footer"></div>
+    </body>
+</html>
